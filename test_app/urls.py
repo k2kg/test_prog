b8767fb0be36f0d2d7bfa5553794b24fb0c0c14a
Dmitry <imp17487@icloud.com>
2024-10-26 18:46:25 +0500

    Add home view and template

--- test_app/urls.py
+++ test_app/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quiz/', include('quiz.urls')),
+    path('<int:test_id>/', views.take_test, name='take_test')
 ]
